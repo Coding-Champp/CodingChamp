7b3e8b1c2b4fed1ab1e55f494023fd553d93826d
Admin <desk.atharv16@gmail.com>
2023-08-25 10:07:10 -0700

    1st Commit

--- app/src/main/java/com/example/codingchamp/MainActivity.java
+++ app/src/main/java/com/example/codingchamp/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.codingchamp;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,12 +22,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        b1 = findViewById(R.id.b1);
-        b2 = findViewById(R.id.b2);
-        b3 = findViewById(R.id.b3);
-        b4 = findViewById(R.id.b4);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        b1 = findViewById(R.id.b1);
+//        b2 = findViewById(R.id.b2);
+//        b3 = findViewById(R.id.b3);
+//        b4 = findViewById(R.id.b4);
+//
         mediaPlayer = MediaPlayer.create(this, R.raw.hindicopy);
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         int currentRingerMode = audioManager.getRingerMode();
@@ -37,42 +36,41 @@ public class MainActivity extends AppCompatActivity {
             audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
         }
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               b1.setCardBackgroundColor(Color.RED);
-               b2.setCardBackgroundColor(Color.WHITE);
-               b4.setCardBackgroundColor(Color.WHITE);
-
-               b3.setCardBackgroundColor(Color.GREEN);
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                b2.setCardBackgroundColor(Color.RED);
-                b1.setCardBackgroundColor(Color.WHITE);
-                b4.setCardBackgroundColor(Color.WHITE);
-                b3.setCardBackgroundColor(Color.GREEN);
-            }
-        });
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                b3.setCardBackgroundColor(Color.GREEN);
-            }
-        });
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                b4.setCardBackgroundColor(Color.RED);
-                b1.setCardBackgroundColor(Color.WHITE);
-                b2.setCardBackgroundColor(Color.WHITE);
-                b3.setCardBackgroundColor(Color.GREEN);
-            }
-        });
-
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//               b1.setCardBackgroundColor(Color.RED);
+//               b2.setCardBackgroundColor(Color.WHITE);
+//               b4.setCardBackgroundColor(Color.WHITE);
+//
+//               b3.setCardBackgroundColor(Color.GREEN);
+//            }
+//        });
+//        b2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                b2.setCardBackgroundColor(Color.RED);
+//                b1.setCardBackgroundColor(Color.WHITE);
+//                b4.setCardBackgroundColor(Color.WHITE);
+//                b3.setCardBackgroundColor(Color.GREEN);
+//            }
+//        });
+//        b3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                b3.setCardBackgroundColor(Color.GREEN);
+//            }
+//        });
+//        b4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                b4.setCardBackgroundColor(Color.RED);
+//                b1.setCardBackgroundColor(Color.WHITE);
+//                b2.setCardBackgroundColor(Color.WHITE);
+//                b3.setCardBackgroundColor(Color.GREEN);
+//            }
+//        });
 
 
     }
